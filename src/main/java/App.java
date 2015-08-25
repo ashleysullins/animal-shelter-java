e7cef3e516c2bbaa3ce8b6e9156bd7d56f0c5a2f
Momo Ozawa & Ashley Sullins <student@epicodus.com>
2015-08-25 16:27:05 -0700

    add integration tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
+import java.time.LocalDate;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -17,6 +18,15 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/animal/info/:id", (request,response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/animalid.vtl");
+
+      Animal animalName = Animal.find(Integer.parseInt(request.queryParams(":id")));
+      model.put("animal", animalName);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/admin", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/admin.vtl");
@@ -24,23 +34,22 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-  //   post("/add-animal", (request, response) -> {
-  //     Map<String, Object> model = new HashMap<String,Object>();
-  //     model.put("template", "templates/home.vtl");
-  //
-  //     String name = request.queryParams("animalName");
-  //     String gender = request.queryParams("animalGender");
-  //     int age = Integer.parseInt(request.queryParams("animalAge"));
-  //     int breed_id = Integer.parseInt(request.queryParams("animalBreed"));
-  //     int species_id = Integer.parseInt(request.queryParams("species"));
-  //
-  //     Animal newAnimal = new Animal(name, gender, age);
-  //     newAnimal.save();
-  //     newAnimal.setBreed(breed_id);
-  //     newAnimal.setSpecies(species_id);
-  //
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
+    post("/add-animal", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/home.vtl");
+
+      String name = request.queryParams("animalName");
+      String gender = request.queryParams("animalGender");
+      int age = Integer.parseInt(request.queryParams("animalAge"));
+      int breed_id = Integer.parseInt(request.queryParams("animalBreed"));
+      int species_id = Integer.parseInt(request.queryParams("species"));
+      model.put("animals", Animal.all());
+
+      Animal newAnimal = new Animal(name, gender, age, breed_id, species_id, LocalDate.now());
+      newAnimal.save();
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   //Algorithm goes here
